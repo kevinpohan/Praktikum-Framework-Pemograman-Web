2077ddef46742c9da6f2033d0635b07496531790
kevinpohan <kevinpohan11@gmail.com>
2025-10-09 12:45:28 +0700

    Praktikum 1 - 7

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/product', action: [ProductController::class, 'index'])->name('product-index');
+Route::get('/product/create', action: [ProductController::class, 'create'])->name('product-create');
+Route::post('/product', action: [ProductController::class, 'store'])->name('product-store');
+Route::get('/product/{id}', action: [ProductController::class, 'show']);
+Route::get('/product/{id}/edit', action: [ProductController::class, 'edit'])->name('product-edit');
+Route::put('/product/{id}', action: [ProductController::class, 'update'])->name('product-update');
+Route::delete('/product/{id}', action: [ProductController::class, 'destroy']);
+
+require __DIR__ . '/auth.php';
